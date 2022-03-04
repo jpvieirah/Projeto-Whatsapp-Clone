4aa6865772e3feabbbd7c548dc2719f3d9d945ed
helsin <jp201101@gmail.com>
2022-03-03 21:27:58 -0300

    Criando evento para o visualizar o tempo de gravação

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -82,6 +82,8 @@ export class MicrophoneController extends ClassEvent {
            });
 
            this._mediaRecorder.start();
+           this.startTimer();
+
 
         }
 
@@ -93,8 +95,27 @@ export class MicrophoneController extends ClassEvent {
 
             this._mediaRecorder.stop();
             this.stop(); 
+            this.stopTimer();
 
         }
+
+    }
+
+    startTimer() {
+
+        let start = Date.now();
+
+        this._recordMicrophoneInterval = setInterval(()=>{
+
+            this.trigger('recordtimer', (Date.now() - start));
+
+        }, 100);
+
+    }
+
+    stopTimer(){
+
+        clearInterval(this._recordMicrophoneInterval);
 
     }
 
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -371,7 +371,7 @@ export class WhatsAppController {
 
                 this.el.recordMicrophone.show();
                 this.el.btnSendMicrophone.hide();
-                this.startRecordMicrophoneTime();
+                
 
                 this._microphoneController = new MicrophoneController();
 
@@ -382,6 +382,13 @@ export class WhatsAppController {
                     this._microphoneController.startRecorder();
 
                 });
+
+                this._microphoneController.on('recordtimer', timer => {
+
+                    this.el.recordMicrophoneTimer.innerHTML = Format.toTime(timer);
+
+                });
+
             });
 
             this.el.btnCancelMicrophone.on('click', e => {
@@ -482,23 +489,11 @@ export class WhatsAppController {
 
     }
 
-    startRecordMicrophoneTime() {
-
-        let start = Date.now();
-
-        this._recordMicrophoneInterval = setInterval(()=>{
-
-            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
-
-        }, 100);
-
-    }
-
     closeRecordMicrophone(){
 
         this.el.recordMicrophone.hide();
         this.el.btnSendMicrophone.show();
-        clearInterval(this._recordMicrophoneInterval);
+        
 
     }
 
